d57bfc6d2cb006ab7682276a2e323d38e273ec23
PATOOWORLD <samuel@patooworld.com>
2024-05-23 01:29:23 -0700

    Update auth.ts
    Signed-off-by: PATOOWORLD <samuel@patooworld.com>

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -1,23 +1,11 @@
-import type {
-  GetServerSidePropsContext,
-  NextApiRequest,
-  NextApiResponse,
-} from "next"
-import type { NextAuthOptions } from "next-auth"
-import { getServerSession } from "next-auth"
-
-// You'll need to import and pass this
-// to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
-export const config = {
-  providers: [], // rest of your config
-} satisfies NextAuthOptions
-
-// Use it in server contexts
-export function auth(
-  ...args:
-    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
-    | [NextApiRequest, NextApiResponse]
-    | []
-) {
-  return getServerSession(...args, config)
-}
+// auth.ts
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+export const { auth, handlers } = NextAuth({ providers: [GitHub] })
+ 
+// middleware.ts
+export { auth as middleware } from "@/auth"
+ 
+// app/api/auth/[...nextauth]/route.ts
+import { handlers } from "@/auth"
+export const { GET, POST } = handlers
